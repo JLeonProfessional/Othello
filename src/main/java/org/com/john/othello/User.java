99588cc7cd3762c69c5f83a0c5555d70adae714d
John Leon <jleonprofessional@gmail.com>
2018-09-01 20:50:17 -0500

    Added wins column to database and lookup

--- src/main/java/org/com/john/othello/User.java
+++ src/main/java/org/com/john/othello/User.java
@@ -10,17 +10,25 @@ import java.sql.Statement;
 public class User {
 	private Integer id;
 	private String name;
+	private int wins;
 
 	public User(String name) {
 		new User(null, name);
 	}
+	
 	public User(Integer id, String name) {
+		this(id, name, 0);
+	}
+
+	public User(Integer id, String name, int wins) {
 		this.id = id;
 		this.name = name;
+		this.wins = wins;
 	}
 
 	public static User lookupUserByName(String name) throws SQLException {
 		Integer id = null;
+		int wins = 0;
 		ResultSet rs = null;
 		if(existsInDatabase(name)) {
 			rs = findUserByName(name);
@@ -31,8 +39,9 @@ public class User {
 		if(rs.next()) {
 			id = rs.getInt("userID");
 			name = rs.getString("userName");
+			wins = rs.getInt("wins");
 		}
-		return new User(id, name);
+		return new User(id, name, wins);
 	}
 
 	private static ResultSet findUserByName(String name) throws SQLException {
@@ -78,6 +87,22 @@ public class User {
 	
 	public void setID(Integer id) {
 		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+
+	public void setWins(int wins) {
+		this.wins = wins;
 	}
 
 }
